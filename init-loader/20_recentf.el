30aef1bb0bcd726a9459110a8426d58985c5f2aa
m-yuya <cir9le@gmail.com>
2016-05-19 16:48:13 +0900

    Change recentf

--- init-loader/20_recentf.el
+++ init-loader/20_recentf.el
@@ -2,15 +2,19 @@
 ;; package @ recentf
 ;; ----------------------------------------------------
 
+(require 'recentf)
+(require 'recentf-ext)
+
 ;; 最近使ったファイルの表示
 (recentf-mode t)
 
 ;; key-bind
 (bind-key* "C-x f" 'recentf-open-files)
 
-(require 'recentf)
-(setq recentf-save-file "~/.recentf")
+;; ~/.emacs.d内に履歴ファイルを保存
+(setq recentf-save-file "~/.emacs.d/.recentf")
+(recentf-load-list)
+
 (setq recentf-max-saved-items 1000)            ;; recentf に保存するファイルの数
-(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
 (setq recentf-auto-cleanup 10)                 ;; 保存する内容を整理
-(run-with-idle-timer 60 t 'recentf-save-list)  ;; 30秒ごとに .recentf を保存
+(run-with-idle-timer 60 t 'recentf-save-list)  ;; 60秒ごとに .recentf を保存
