e0a0d2e79a4156ee096522b60b6639ef5a4454b5
m-yuya <cir9le@gmail.com>
2016-05-19 16:44:02 +0900

    Add recentf-ext

---
+++ elpa/recentf-ext-20130130.1350/recentf-ext-autoloads.el
@@ -0,0 +1,16 @@
+;;; recentf-ext-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("recentf-ext.el") (22333 25480 916155
+;;;;;;  400000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; recentf-ext-autoloads.el ends here
